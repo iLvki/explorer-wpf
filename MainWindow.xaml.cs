c90c4860655df6d143546084ce91c3bcc8755530
Livki <115584410+iLvki@users.noreply.github.com>
2025-02-21 10:21:09 +0100

    Add project files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace odkrywca
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Page
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mapBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //NavigationService.Navigate(new Uri("/Pages/Map.xaml", UriKind.Relative));
+        }
+
+        private void closeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
