3ea83063deb8a24854aad930006c01e687663f25
Livki <115584410+iLvki@users.noreply.github.com>
2025-02-23 17:10:20 +0100

    Dodanie grafik

--- Pages/Map.xaml.cs
+++ Pages/Map.xaml.cs
@@ -24,5 +24,25 @@ namespace odkrywca.Pages
         {
             InitializeComponent();
         }
+
+        private void changeImage(object sender, MouseEventArgs e)
+        {
+            string continent = (sender as Hyperlink).Name;
+
+            Uri imgUri = new Uri($"Assets/Hovers/{continent}-h.png", UriKind.Relative);
+
+            map.Background = new ImageBrush
+            {
+                ImageSource = new BitmapImage(imgUri)
+            };
+        }
+
+        private void BackToBaseImg(object sender, MouseEventArgs e)
+        {
+            map.Background = new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri($"Assets/continents.png", UriKind.Relative))
+            };
+        }
     }
 }
